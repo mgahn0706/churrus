4f6233670af644244d437f0e5d0a8c2202131667
안민규 <mgahn0706@snu.ac.kr>
2023-05-20 03:19:05 +0900

    fix: delete isLoading state in main page

--- suspect-game/src/pages/startup/index.tsx
+++ suspect-game/src/pages/startup/index.tsx
@@ -34,7 +34,6 @@ export default function Startup() {
     "rule" | "prologue" | "suspects" | "dashboard" | "password" | null
   >("rule");
   const [unlockingClue, setUnlockingClue] = useState<ClueType | null>(null);
-  const [isImageLoading, setImageLoading] = useState(false);
 
   const handleCloseModal = () => {
     setOpenedModal(null);
@@ -92,12 +91,10 @@ export default function Startup() {
     );
   }
 
+
   return (
     <Box>
       <Image
-        loading={isImageLoading}
-        onLoadingComplete={()=>setImageLoading(false)}
-        onLoadStart={()=>setImageLoading(true)}
         src={`/image/map/startup-${currentPlace}.png`}
         alt="스타트업 맵 이미지"
         fill
