84ef1a93b10f06013595ac9663391a346c1e74e2
Force, Mingyu <mgahn0706@snu.ac.kr>
2023-06-04 01:41:45 +0900

    feat: add scenario background image

--- suspect-game/src/fixtures/index.ts
+++ suspect-game/src/fixtures/index.ts
@@ -1,60 +1,55 @@
-export const scenarios : ScenarioType[]= [{
-    title: '스타트업 살인사건',
+export const scenarios: ScenarioType[] = [
+  {
+    title: "스타트업 살인사건",
     difficulty: 2,
     numberOfSuspects: 3,
     playTime: 90,
-    backgroundImage: '/image/scenario/startup-main.png',
-    cardImage: '/Suspect_Logo.png',
-    keyword: 'startup',
+    backgroundImage: "/image/scenario/startup-main.png",
+    cardImage: "/Suspect_Logo.png",
+    keyword: "startup",
     isInDevelopment: false,
-    bgmURL: 'https://www.youtube.com/watch?v=qMwzWk81tVM',
-
-}
-,
-{
-    title: '자하연 살인사건',
+    bgmURL: "https://www.youtube.com/watch?v=qMwzWk81tVM",
+  },
+  {
+    title: "자하연 살인사건",
     difficulty: 3,
     numberOfSuspects: 4,
     playTime: 120,
-    backgroundImage: '/Suspect_Logo.png',
-    cardImage: '/Suspect_Logo.png',
-    keyword: 'jahayeon',
+    backgroundImage: "/image/scenario/jahayeon-main.png",
+    cardImage: "/Suspect_Logo.png",
+    keyword: "jahayeon",
     isInDevelopment: true,
-
-}
-,
-{
-    title: '추러스 MT 살인사건',
+  },
+  {
+    title: "추러스 MT 살인사건",
     difficulty: 1,
     numberOfSuspects: 3,
     playTime: 120,
-    backgroundImage: '/Suspect_Logo.png',
-    cardImage: '/Suspect_Logo.png',
-    keyword: 'mt',
+    backgroundImage: "/image/scenario/mt-main.png",
+    cardImage: "/Suspect_Logo.png",
+    keyword: "mt",
     isInDevelopment: true,
-
-},
-{
-    title: '와부고 살인사건',
+  },
+  {
+    title: "와부고 살인사건",
     difficulty: 4,
     numberOfSuspects: 4,
     playTime: 150,
-    backgroundImage: '/Suspect_Logo.png',
-    cardImage: '/Suspect_Logo.png',
-    keyword: 'highschool',
+    backgroundImage: "/image/scenario/highschool-main.png",
+    cardImage: "/Suspect_Logo.png",
+    keyword: "highschool",
     isInDevelopment: true,
-
-}
-]
+  },
+];
 
 export interface ScenarioType {
-    title: string;
-    difficulty: number;
-    numberOfSuspects: number;
-    playTime: number;
-    backgroundImage: string;
-    cardImage: string;
-    keyword: string;
-    isInDevelopment: boolean;
-    bgmURL?: string;
+  title: string;
+  difficulty: number;
+  numberOfSuspects: number;
+  playTime: number;
+  backgroundImage: string;
+  cardImage: string;
+  keyword: string;
+  isInDevelopment: boolean;
+  bgmURL?: string;
 }
