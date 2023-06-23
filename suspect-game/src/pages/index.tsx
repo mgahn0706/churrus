f760f04accb688315e6edce7dd3806e20ecaf33c
Force, Mingyu <mgahn0706@snu.ac.kr>
2023-06-24 02:04:20 +0900

    fix: add color to landing page

--- suspect-game/src/pages/index.tsx
+++ suspect-game/src/pages/index.tsx
@@ -1,10 +1,21 @@
 import { useMobileWidth } from "@/hooks/useMobileWIdth";
 import { useResponsiveValue } from "@/hooks/useResponsiveValue";
 import { useWindowSize } from "@/hooks/useWindowSize";
-import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { useRouter } from "next/router";
 import { useMemo } from "react";
 
 interface ContentType {
+  category: string;
   title: string;
   description: string;
   url: string;
@@ -13,36 +24,42 @@ interface ContentType {
 
 const contents: ContentType[] = [
   {
+    category: "추리",
     title: "협동 크라임씬",
-    description: "",
+    description: "다함께 용의자들 중 범인을 추리하세요.",
     url: "suspect",
-    color: "red",
+    color: "#FF0060",
   },
   {
+    category: "더 지니어스",
     title: "마이너스 경매",
     description: "",
     url: "auction",
-    color: "blue",
+    color: "#F6FA70",
   },
   {
+    category: "더 지니어스",
     title: "미스터리 사인",
     description: "",
     url: "mystery-sign",
-    color: "green",
+    color: "#00DFA2",
   },
   {
+    category: "문제풀이",
     title: "문제적 추러스",
     description: "",
     url: "problematic",
-    color: "yellow",
+    color: "#0079FF",
   },
   {
+    category: "추리",
     title: "장편 추리 문제",
     description: "",
     url: "murder-mystery",
     color: "",
   },
   {
+    category: "더 지니어스",
     title: "체인옥션",
     description: "",
     url: "chain-auction",
@@ -54,14 +71,43 @@ export default function Churrus() {
   const responsiveXS = useResponsiveValue([12, 6, 4]);
 
   const ContentCard = ({ content }: { content: ContentType }) => {
+    const router = useRouter();
     return (
       <Grid item xs={responsiveXS}>
         <Card
+          variant="outlined"
           sx={{
+            transition: "all 0.2s",
             height: 200,
+            boxShadow: `#3b82f6 0px 3px 8px`,
+            "&:hover": {
+              border: `#0f0f70 solid 2px`,
+              boxShadow: `#3b82f6 0px 5px 8px`,
+            },
           }}
         >
-          <CardContent>{content.title}</CardContent>
+          <CardContent
+            onClick={() => {
+              router.push(`/${content.url}`);
+            }}
+            sx={{
+              height: 200,
+            }}
+          >
+            {" "}
+            <Typography
+              variant="h5"
+              component="div"
+              color="#3b82f6"
+              fontWeight="bold"
+              gutterBottom
+            >
+              {content.title}
+            </Typography>
+            <Typography variant="body2" color="#3b82f6">
+              {content.description}
+            </Typography>
+          </CardContent>
         </Card>
       </Grid>
     );
