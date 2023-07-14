ef6485d98a935905b812fe26c300be1419dd8580
Force, Mingyu <mgahn0706@snu.ac.kr>
2023-07-14 13:13:33 +0900

    feat: 마이너스 경매 배포

--- suspect-game/src/features/minus-auction/components/CubeInAuction.tsx
+++ suspect-game/src/features/minus-auction/components/CubeInAuction.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
 
-export default function CubInAuction({
+export default function CubeInAuction({
   children,
 }: {
   children: React.ReactNode;
--- suspect-game/src/features/minus-auction/components/HiddenCudeBidModal.tsx
+++ suspect-game/src/features/minus-auction/components/HiddenCudeBidModal.tsx
@@ -8,7 +8,9 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  Switch,
   TextField,
+  Typography,
 } from "@mui/material";
 import { PlayerType } from "../types";
 import { useState } from "react";
@@ -26,27 +28,30 @@ export default function HiddenCubeBidModal({
   players: PlayerType[];
   onBid: (player: PlayerType, bid: number) => void;
 }) {
-  const [bider, setBider] = useState<PlayerType | null>(null);
+  const [bider, setBider] = useState<string>("");
   const [finalChip, setFinalChip] = useState<number>(0);
+  const [isIncludingHiddenCube, setIsIncludingHiddenCube] =
+    useState<boolean>(false);
 
   return (
     <Dialog open={isOpen}>
       <DialogTitle>히든큐브 경매</DialogTitle>
       <DialogContent>
-        <DialogContentText>
+        <DialogContentText mb={5}>
           히든큐브를 확인하고 게임 포함 여부를 선택할 수 있는 권리에 대한 경매가
           진행됩니다.
         </DialogContentText>
-        <InputLabel id="hidden-bidder-label">경매 낙찰자</InputLabel>
         <Select
-          labelId="hidden-bidder-label"
           id="demo-simple-select"
+          sx={{
+            width: "300px",
+            mr: 3,
+          }}
+          autoWidth
           value={bider}
-          label="경매 낙찰자"
+          label="Bider"
           onChange={(e) => {
-            setBider(
-              players.filter((player) => player.name === e.target.value)[0]
-            );
+            setBider(e.target.value as string);
           }}
         >
           {players.map((player) => (
@@ -54,19 +59,41 @@ export default function HiddenCubeBidModal({
           ))}
         </Select>
         <TextField
+          label="최종 낙찰가"
           id="hidden-bidder-label"
           value={finalChip}
           type="number"
           onChange={(e) => {
             setFinalChip(Number(e.target.value));
           }}
+        />
+      </DialogContent>
+      <DialogContent>
+        <InputLabel id="is-including">히든 큐브 포함 여부</InputLabel>
+        <Switch
+          value={isIncludingHiddenCube}
+          onChange={(e) => {
+            setIsIncludingHiddenCube(e.target.checked);
+          }}
+        />
+        <Typography
+          variant="body2"
+          color={isIncludingHiddenCube ? "primary.main" : "gray"}
+          ml={2}
         >
-          경매 낙찰가
-        </TextField>
+          {isIncludingHiddenCube ? "포함" : "제외"}
+        </Typography>
       </DialogContent>
       <DialogActions>
         <Button
+          disabled={!bider || !finalChip}
           onClick={() => {
+            onBid(
+              players.find((player) => {
+                return player.name === bider;
+              })!,
+              finalChip
+            );
             onClose();
           }}
         >
