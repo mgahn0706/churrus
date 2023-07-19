a947b1521380d597eb40c4988c86bf67cead4d8e
Force, Mingyu <mgahn0706@snu.ac.kr>
2023-07-19 17:19:28 +0900

    feat: 내마음의 주파수 1차 배포

--- suspect-game/src/features/frequency/components/Dial.tsx
+++ suspect-game/src/features/frequency/components/Dial.tsx
@@ -1,14 +1,15 @@
 import { Box, Button } from "@mui/material";
 import { useEffect, useState } from "react";
 
-export default function Dial({ isCoverRemoved }: { isCoverRemoved: boolean }) {
+export default function Dial({
+  isCoverRemoved,
+  exactDegree,
+}: {
+  isCoverRemoved: boolean;
+  exactDegree: number;
+}) {
   // exact dial can be 1 to 14.
-  const [exactDegree, setExactDegree] = useState(0);
   const [dialDegree, setDialDegree] = useState(0);
-
-  useEffect(() => {
-    setExactDegree(Math.floor(Math.random() * 14));
-  }, []);
 
   return (
     <>
@@ -16,67 +17,74 @@ export default function Dial({ isCoverRemoved }: { isCoverRemoved: boolean }) {
         <Box
           height="300px"
           width="600px"
-          bgcolor={!isCoverRemoved ? "#D1CBA7" : "#62afa8"}
+          bgcolor={isCoverRemoved ? "#D1CBA7" : "#62afa8"}
           borderRadius="300px 300px 0 0"
-        />
-        <Box
-          width="7px"
-          zIndex={4}
-          height="200px"
-          bgcolor="red"
-          position="absolute"
-          borderRadius={3}
-          left="50%"
-          bottom="49%"
-          sx={{
-            transform: `rotate(${dialDegree}deg)`,
-            transformOrigin: "bottom center",
-            transition: "transform 0.5s ease",
-          }}
-        />
-        <Box
-          position="absolute"
-          zIndex={3}
-          sx={{
-            width: "600px",
-            height: "600px",
-            borderRadius: "50%",
-            background:
-              "linear-gradient(12deg, #A8CCE1 17.53%, transparent 17.53%) 0 0",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "50% 50%",
-            transform: `rotate(${exactDegree * 12}deg)`,
-          }}
-        />
-        <Box
-          position="absolute"
-          zIndex={2}
-          sx={{
-            width: "600px",
-            height: "600px",
-            borderRadius: "50%",
-            background:
-              "linear-gradient(36deg, #FA6328 42.34%, transparent 42.34%) 0 0",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "50% 50%",
-            transform: `rotate(${(exactDegree - 1) * 12}deg)`,
-          }}
-        />
-        <Box
-          position="absolute"
-          zIndex={1}
-          sx={{
-            width: "600px",
-            height: "600px",
-            borderRadius: "50%",
-            background:
-              "linear-gradient(60deg, #FBBB0A 63.4%, transparent 63.4%) 0 0",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "50% 50%",
-            transform: `rotate(${(exactDegree - 2) * 12}deg)`,
-          }}
-        />
+        >
+          <Box
+            width="7px"
+            zIndex={4}
+            height="200px"
+            bgcolor="red"
+            position="absolute"
+            borderRadius={3}
+            left="50%"
+            bottom="46%"
+            sx={{
+              transform: `rotate(${dialDegree}deg)`,
+              transformOrigin: "bottom center",
+              transition: "transform 0.5s ease",
+            }}
+          />
+
+          {isCoverRemoved && (
+            <>
+              <Box
+                position="absolute"
+                zIndex={3}
+                sx={{
+                  width: "600px",
+                  height: "600px",
+                  borderRadius: "50%",
+                  background:
+                    "linear-gradient(12deg, #A8CCE1 17.53%, transparent 17.53%) 0 0",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "50% 50%",
+                  transform: `rotate(${exactDegree * 12}deg)`,
+                }}
+              />
+              <Box
+                position="absolute"
+                zIndex={2}
+                sx={{
+                  width: "600px",
+                  height: "600px",
+                  borderRadius: "50%",
+                  background:
+                    "linear-gradient(36deg, #FA6328 42.34%, transparent 42.34%) 0 0",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "50% 50%",
+                  transform: `rotate(${(exactDegree - 1) * 12}deg)`,
+                }}
+              />
+              <Box
+                position="absolute"
+                zIndex={1}
+                sx={{
+                  width: "600px",
+                  height: "600px",
+                  borderRadius: "50%",
+                  background:
+                    "linear-gradient(60deg, #FBBB0A 63.4%, transparent 63.4%) 0 0",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "50% 50%",
+                  transform: `rotate(${(exactDegree - 2) * 12}deg)`,
+                }}
+              />
+            </>
+          )}
+        </Box>
       </Box>
+
       <Box
         display="flex"
         justifyContent="center"
--- suspect-game/src/pages/frequency/index.tsx
+++ suspect-game/src/pages/frequency/index.tsx
@@ -1,11 +1,12 @@
 import HomeButton from "@/components/HomeButton";
 import { useEffect, useState } from "react";
 
-import { Box, Card, Typography } from "@mui/material";
+import { Box, Button, Card, IconButton, Typography } from "@mui/material";
 import { ConceptType } from "@/features/frequency/types";
 import ConceptCard from "@/features/frequency/components/ConceptCard";
 import { FrequencyConcepts } from "@/features/frequency/fixtures";
 import Dial from "@/features/frequency/components/Dial";
+import { SkipNext } from "@mui/icons-material";
 
 export default function Frequency() {
   const [round, setRound] = useState(1);
@@ -29,7 +30,7 @@ export default function Frequency() {
     right: "",
   });
 
-  const [dial, setDial] = useState(8);
+  const [exactDegree, setExactDegree] = useState(0);
 
   const [defenderPrediction, setDefenderPrediction] = useState<
     "left" | "right"
@@ -38,21 +39,47 @@ export default function Frequency() {
   const [playedConcepts, setPlayedConcepts] = useState<ConceptType[]>([]);
 
   useEffect(() => {
+    setPlayedConcepts([...playedConcepts, concept]);
     const randomNumber = Math.floor(Math.random() * FrequencyConcepts.length);
-    setConcept(FrequencyConcepts[randomNumber]);
-  }, []);
+    setConcept(
+      playedConcepts.includes(FrequencyConcepts[randomNumber])
+        ? FrequencyConcepts[randomNumber + 1]
+        : FrequencyConcepts[randomNumber]
+    );
+    setExactDegree(Math.floor(Math.random() * 14));
+  }, [round]);
 
   return (
     <Box textAlign="center" pt={4}>
       <HomeButton />
 
       <Typography variant="h5">내 마음의 주파수</Typography>
-      <Typography variant="h6">Round {round}</Typography>
-      <Dial isCoverRemoved={phase === "guess_dial"} />
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Typography variant="h6">Round {round}</Typography>
+        <IconButton
+          sx={{
+            ml: 2,
+          }}
+          onClick={() => {
+            setRound(round + 1);
+          }}
+        >
+          <SkipNext />
+        </IconButton>
+      </Box>
 
-      <Box display="flex" justifyContent="center" mt="100px">
+      <Dial isCoverRemoved={phase === "guess_dial"} exactDegree={exactDegree} />
+
+      <Box display="flex" justifyContent="center" mt="140px" mb={2}>
         <ConceptCard concept={concept} />
       </Box>
+      <Button
+        onClick={() => {
+          setPhase(phase === "guess_dial" ? "IDLE" : "guess_dial");
+        }}
+      >
+        {phase === "guess_dial" ? "다이얼 가리기" : "다이얼 열기"}
+      </Button>
     </Box>
   );
 }
