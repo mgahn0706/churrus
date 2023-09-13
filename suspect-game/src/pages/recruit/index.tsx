c8c518dd4ceb482a2ddff1545a4ba85398db1b12
Force, Mingyu <mgahn0706@snu.ac.kr>
2023-09-13 17:55:46 +0900

    fix: fix recruit page

--- suspect-game/src/pages/recruit/index.tsx
+++ suspect-game/src/pages/recruit/index.tsx
@@ -9,13 +9,12 @@ import {
   Divider,
   Typography,
 } from "@mui/material";
-import Link from "next/link";
 
 const FAQList: Record<string, string> = {
   "추러스는 교내 동아리인가요?":
     "네, 서울대학교 교내동아리입니다. 현재 중앙동아리 가등록 상태입니다.",
   "가입비가 있나요?":
-    "네, 있습니다. 가입비 20000원을 받고 있으며, 정기모임 준비비용, 방탈출 예약금 (보증금 형식으로 방탈출을 예약할 때 예약금을 요구하는 경우가 있으며, 방탈출 종료 후 반환됩니다), 뒷풀이 지원, 엠티 경품 등등으로 사용되며 매 사용 시마다 투명하게 공개됩니다.",
+    "네, 있습니다. 가입비 20,000원을 받고 있으며, 정기모임 준비비용, 방탈출 예약금 (보증금 형식으로 방탈출을 예약할 때 예약금을 요구하는 경우가 있으며, 방탈출 종료 후 반환됩니다), 뒷풀이 지원, 엠티 경품 등등으로 사용되며 매 사용 시마다 투명하게 공개됩니다.",
   "고학번 화석인데 들어가도 괜찮나요?":
     "저번 회기 기준 학번 중앙값이 22학번이었으나, 11학번부터 23학번까지 다양하게 분포되어 있습니다. 대학원생도 상당수 분포되어 있으니 부담없이 지원하셔도 됩니다!",
   "추리를 못해도 괜찮나요?":
@@ -34,7 +33,7 @@ export default function RecruitPage() {
             fontWeight: 700,
           }}
         >
-          {"지금은 모집기간이 아니에요 :("}
+          지금은 모집기간이 아니에요
         </Box>
         <Box fontSize={"1.5rem"}>
           다음 학기 모집은 2024년 3월에 진행할 예정이에요. <br />
@@ -99,6 +98,7 @@ export default function RecruitPage() {
           {Object.entries(FAQList).map(([question, answer]) => {
             return (
               <Accordion
+                key={question}
                 sx={{
                   width: "80vw",
                   textAlign: "left",
@@ -123,6 +123,7 @@ export default function RecruitPage() {
                 </AccordionSummary>
                 <AccordionDetails
                   sx={{
+                    wordBreak: "keep-all",
                     px: 3,
                   }}
                 >
