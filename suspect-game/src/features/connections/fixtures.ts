a6a82c37da5c1a2ca660a2de8a3fee2ff8743357
Mingyu <mgahn0706@snu.ac.kr>
2023-09-28 16:45:09 +0900

    fix: add connection puzzles

--- suspect-game/src/features/connections/fixtures.ts
+++ suspect-game/src/features/connections/fixtures.ts
@@ -63,6 +63,258 @@ export const KoreanConnections: ConnectionsType[] = [
       },
     ],
   },
+  {
+    id: 4,
+    quiz: [
+      {
+        words: ["자두", "살구", "복숭아", "아보카도"],
+        description: "하나의 큰 씨가 있는 과일",
+      },
+      {
+        words: ["고추장", "케첩", "살사", "베샤멜"],
+        description: "소스",
+      },
+      {
+        words: ["DNA", "다이너마이트", "버터", "아이돌"],
+        description: "BTS의 노래 제목",
+      },
+      {
+        words: ["슈퍼", "포터", "배트", "팩"],
+        description: "___맨",
+      },
+    ],
+  },
+  {
+    id: 5,
+    quiz: [
+      {
+        words: ["위", "간", "이자", "기도"],
+        description: "신체 기관",
+      },
+      {
+        words: ["장구", "북", "공", "오르골"],
+        description: "타악기",
+      },
+      {
+        words: ["시트러스", "머스크", "우디", "허브"],
+        description: "향수의 향",
+      },
+      {
+        words: ["갈라파고스", "솔로몬", "쿡", "카나리아"],
+        description: "제도",
+      },
+    ],
+  },
+  {
+    id: 6,
+    quiz: [
+      {
+        words: ["홀덤", "방탈출", "퀴즈", "소설"],
+        description: "추러스 활동",
+      },
+      {
+        words: ["얼음", "김", "눈", "깅"],
+        description: "물의 형태",
+      },
+      {
+        words: ["구설수", "왕위", "산", "궤도"],
+        description: "오를 수 있는 것",
+      },
+      {
+        words: ["컵", "비행기", "빨대", "호랑이"],
+        description: "종이 ___",
+      },
+    ],
+  },
+  {
+    id: 7,
+    quiz: [
+      {
+        words: ["중국", "스페인", "아이티", "수단"],
+        description: "국가",
+      },
+      {
+        words: ["디스코", "스트릿", "포크", "포고"],
+        description: "춤",
+      },
+      {
+        words: ["로퍼", "하이힐", "옥스퍼드", "메리제인"],
+        description: "구두",
+      },
+      {
+        words: ["골", "윙", "크로스", "티키타카"],
+        description: "축구 용어",
+      },
+    ],
+  },
+  {
+    id: 8,
+    quiz: [
+      {
+        words: ["휘발유", "아스팔트", "비닐", "플라스틱"],
+        description: "석유를 원료로 하는 것",
+      },
+      {
+        words: ["비", "피", "오", "아이"],
+        description: "알파벳",
+      },
+      {
+        words: ["붓꽃", "해바라기", "자화상", "모나리자"],
+        description: "명화",
+      },
+      {
+        words: ["심판", "달", "연인", "악마"],
+        description: "타로카드",
+      },
+    ],
+  },
+  {
+    id: 9,
+    quiz: [
+      {
+        words: ["척추", "해골", "갈비", "골반"],
+        description: "인체의 뼈",
+      },
+      {
+        words: ["상아", "진주", "호박", "산호"],
+        description: "유기물로 만들어진 보석",
+      },
+      {
+        words: ["톳", "보루", "판", "축"],
+        description: "단위",
+      },
+      {
+        words: ["마지막", "밥", "손", "홀"],
+        description: "맨___",
+      },
+    ],
+  },
+  {
+    id: 10,
+    quiz: [
+      {
+        words: ["개", "토끼", "원숭이", "용"],
+        description: "십이지신",
+      },
+      {
+        words: ["벽록", "군청", "밤", "갈매"],
+        description: "색",
+      },
+      {
+        words: ["고름", "깃", "소매", "폭"],
+        description: "한복의 부분",
+      },
+      {
+        words: ["생존", "그림", "유전", "화학"],
+        description: "___자",
+      },
+    ],
+  },
+  {
+    id: 11,
+    quiz: [
+      {
+        words: ["무좀", "사마귀", "건선", "티눈"],
+        description: "피부 질환",
+      },
+      {
+        words: ["버섯", "효모", "곰팡이", "동충하초"],
+        description: "균류",
+      },
+      {
+        words: ["비행기", "새", "파리", "선풍기"],
+        description: "날개가 있는 것",
+      },
+      {
+        words: ["전화기", "TV", "텔레파시", "망원경"],
+        description: "영어가 tele___ 인 단어",
+      },
+    ],
+  },
+  {
+    id: 12,
+    quiz: [
+      {
+        words: ["주석", "납", "비소", "수은"],
+        description: "금속 원소",
+      },
+      {
+        words: ["슬림", "스키니", "테이퍼드", "와이드"],
+        description: "바지 팟",
+      },
+      {
+        words: ["벤치", "짐볼", "이령", "랙"],
+        description: "운동기구",
+      },
+      {
+        words: ["아이언", "우드", "웨지", "퍼터"],
+        description: "골프클럽",
+      },
+    ],
+  },
+  {
+    id: 13,
+    quiz: [
+      {
+        words: ["포", "기갑", "총", "리볼버"],
+        description: "현대 무기",
+      },
+      {
+        words: ["루피", "페티", "로디", "해리"],
+        description: "뽀로로의 캐릭터",
+      },
+      {
+        words: ["프랑", "루블", "원", "페소"],
+        description: "통화 단위",
+      },
+      {
+        words: ["푹스", "파인만", "노이만", "보어"],
+        description: "맨해튼 프로젝트의 과학자",
+      },
+    ],
+  },
+  {
+    id: 14,
+    quiz: [
+      {
+        words: ["뱀", "코끼리", "표범", "사자"],
+        description: "바다___ 인 동물",
+      },
+      {
+        words: ["기업", "우리", "하나", "국민"],
+        description: "___은행",
+      },
+      {
+        words: ["동해", "남산", "가을", "기상"],
+        description: "애국가 첫 단어",
+      },
+      {
+        words: ["아파트", "단지", "번지", "호수"],
+        description: "주소",
+      },
+    ],
+  },
+  {
+    id: 15,
+    quiz: [
+      {
+        words: ["강원", "라라", "서울", "디즈니"],
+        description: "___랜드",
+      },
+      {
+        words: ["신", "열", "참깨", "틈새"],
+        description: "___라면",
+      },
+      {
+        words: ["탑", "바다", "유리", "길"],
+        description: "연예인",
+      },
+      {
+        words: ["솔", "파라솔", "도시", "파도"],
+        description: "계이름으로 이루어진 단어",
+      },
+    ],
+  },
 ];
 
 interface ConnectionsType {
