301d59bbf1e58b8d015355f070b9fc1b3a182e54
Mingyu <mgahn0706@snu.ac.kr>
2024-01-21 13:54:17 +0900

    feat: add additional Column

--- home/src/features/hues-and-cues/components/ColorSelectSection.tsx
+++ home/src/features/hues-and-cues/components/ColorSelectSection.tsx
@@ -1,7 +1,6 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { COLORS } from "../fixtures";
 import { getCharCodeFromNumber } from "../libs";
-import { RouteTwoTone } from "@mui/icons-material";
 
 const COLOM_HEADERS = Array.from({ length: 30 }, (_, i) => i + 1);
 
@@ -12,7 +11,7 @@ const ColorPanel = ({
   color: string;
   onClick: () => void;
 }) => (
-  <Grid item xs={0.387}>
+  <Grid item xs={0.375}>
     <Box
       onClick={onClick}
       bgcolor={color}
@@ -36,13 +35,13 @@ export default function ColorSelectSection({
 }) {
   return (
     <Grid container width="80dvw" spacing={1}>
-      <Grid item xs={0.387} />
+      <Grid item xs={0.375} />
 
-      {COLOM_HEADERS.map((colom) => (
+      {COLOM_HEADERS.map((colomn) => (
         <Grid
           item
-          xs={0.387}
-          key={colom}
+          xs={0.375}
+          key={colomn}
           sx={{
             color: "white",
             textAlign: "center",
@@ -50,7 +49,7 @@ export default function ColorSelectSection({
             fontSize: "1.2rem",
           }}
         >
-          {colom}
+          {colomn}
         </Grid>
       ))}
       {Object.values(COLORS).map((colors, row) => {
@@ -58,7 +57,7 @@ export default function ColorSelectSection({
           <>
             <Grid
               item
-              xs={0.387}
+              xs={0.375}
               key={row}
               sx={{
                 alignItems: "center",
@@ -80,9 +79,40 @@ export default function ColorSelectSection({
                 }}
               />
             ))}
+            <Grid
+              item
+              xs={0.375}
+              key={row}
+              sx={{
+                alignItems: "center",
+                display: "flex",
+                justifyContent: "center",
+                color: "white",
+                textAlign: "center",
+                fontWeight: "bolder",
+                fontSize: "1.2rem",
+              }}
+            >
+              {getCharCodeFromNumber(row)}
+            </Grid>
           </>
         );
       })}
+      {COLOM_HEADERS.map((colomn) => (
+        <Grid
+          item
+          xs={0.375}
+          key={colomn}
+          sx={{
+            color: "white",
+            textAlign: "center",
+            fontWeight: "bolder",
+            fontSize: "1.2rem",
+          }}
+        >
+          {colomn}
+        </Grid>
+      ))}
     </Grid>
   );
 }
