28b6c9f3a00232ed72fd50914a420c1cabe4f2f4
Force, Mingyu <mgahn0706@snu.ac.kr>
2024-02-01 19:03:41 +0900

    WIP: 배너 스타일 변경

--- home/src/components/MainBanner.tsx
+++ home/src/components/MainBanner.tsx
@@ -1,9 +1,18 @@
 import { useMobileWidth } from "@/hooks/useMobileWIdth";
 import { PlayArrowSharp } from "@mui/icons-material";
-import { Box, Typography, Button } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardHeader,
+  CardContent,
+  CardMedia,
+  CardActionArea,
+} from "@mui/material";
 import router from "next/router";
 
-const BANNER_COLOR = "white";
+const BANNER_COLOR = "#f96556";
 
 const BANNER_IMAGE = "/image/banner/2023-11.png";
 
@@ -22,133 +31,38 @@ const handleClickBannerButton = () => {
 export default function MainBanner() {
   const { isMobileWidth } = useMobileWidth();
 
-  if (isMobileWidth)
-    return (
-      <Box display="flex" justifyContent="center">
-        <Box
-          display="flex"
-          justifyContent="flex-start"
-          width="90vw"
-          height="350px"
-          borderRadius="16px"
-          sx={{
-            bgcolor: BANNER_COLOR,
-          }}
-        >
-          <Box
-            width="100vw"
-            display="flex"
-            flexDirection="column"
-            pl={4}
-            py={5}
-            justifyContent="flex-start"
-            gap={3}
-          >
-            <Typography color="black" variant="body1" mb={-2}>
-              2023년 11월 정기모임
-            </Typography>
-            <Typography color="black" fontWeight="bold" variant="h4">
-              {BANNER_TEXT.subtitle} &
-            </Typography>
-            <Typography color="#f96556" fontWeight="bold" variant="h4" mt={-3}>
-              {BANNER_TEXT.title}
-            </Typography>
-            <Box display="flex" justifyContent="space-between">
-              <Button
-                variant="contained"
-                sx={{
-                  width: "fit-content",
-                  height: "fit-content",
-                  backgroundColor: "#f96556",
-                  color: "white",
-                  fontWeight: "bold",
-                  "&:hover": {
-                    backgroundColor: "#da5142",
-                  },
-                }}
-                size="small"
-                onClick={handleClickBannerButton}
-              >
-                시작
-                <PlayArrowSharp />
-              </Button>
-              <Box
-                mt={-2}
-                width="75%"
-                height="200px"
-                sx={{
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat",
-                  backgroundPosition: "center",
-                  backgroundImage: `linear-gradient(to bottom, ${BANNER_COLOR} 3%, transparent 20%, transparent 70%, ${BANNER_COLOR} 98%), linear-gradient(to left, ${BANNER_COLOR} 3%, transparent 20%, transparent 80%, ${BANNER_COLOR} 98%), url("${BANNER_IMAGE}") `,
-                }}
-              />
-            </Box>
-          </Box>
-        </Box>
-      </Box>
-    );
-
   return (
     <Box display="flex" justifyContent="center">
       <Box
         display="flex"
-        justifyContent="flex-start"
+        justifyContent="center"
         width="60vw"
         height="40vh"
         borderRadius="16px"
+        alignItems="center"
         sx={{
           bgcolor: BANNER_COLOR,
         }}
       >
-        <Box
-          width="25vw"
-          display="flex"
-          flexDirection="column"
-          px={5}
-          py={5}
-          justifyContent="flex-start"
-          gap={3}
-        >
-          <Typography color="black" variant="h6" mb={-2}>
-            2023년 11월 정기모임
-          </Typography>
-          <Typography color="black" fontWeight="bold" variant="h3">
-            {BANNER_TEXT.subtitle} &
-          </Typography>
-          <Typography color="#f96556" fontWeight="bold" variant="h3" mt={-3}>
-            {BANNER_TEXT.title}
-          </Typography>
-          <Button
-            variant="contained"
-            sx={{
-              width: "fit-content",
-              backgroundColor: "#f96556",
-              color: "white",
-              fontWeight: "bold",
-              fontSize: "1.2rem",
-              "&:hover": {
-                backgroundColor: "#da5142",
-              },
-            }}
-            size="large"
-            onClick={handleClickBannerButton}
-          >
-            시작
-            <PlayArrowSharp />
-          </Button>
-        </Box>
-
-        <Box
-          width="30vw"
-          height="100%"
-          sx={{
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            backgroundImage: `linear-gradient(to bottom, ${BANNER_COLOR} 3%, transparent 20%, transparent 70%, ${BANNER_COLOR} 98%), linear-gradient(to left, ${BANNER_COLOR} 3%, transparent 20%, transparent 80%, ${BANNER_COLOR} 98%), url("${BANNER_IMAGE}") `,
-          }}
-        />
+        <Card sx={{ maxWidth: 600, maxHeight: 350 }}>
+          <CardActionArea>
+            <CardContent>
+              <CardMedia
+                width="50px"
+                height="50px"
+                component="img"
+                image={BANNER_IMAGE}
+                alt="banner"
+              />
+              <Typography gutterBottom variant="h5" component="div">
+                2024년 1월 정기모임
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {BANNER_TEXT.title}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </Box>
     </Box>
   );
