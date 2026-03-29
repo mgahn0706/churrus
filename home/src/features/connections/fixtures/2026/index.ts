effbd6fea8a6fe4c6f8ab6b04992a5830355b6d8
qdrptd <qdrptd@gmail.com>
2026-03-29 19:55:21 +0900

    커넥션 스크립트 제작 및 3~6주차 업로드

--- home/src/features/connections/fixtures/2026/index.ts
+++ home/src/features/connections/fixtures/2026/index.ts
@@ -43,4 +43,88 @@ export const CONNECTIONS2026: ConnectionsType[] = [
       },
     ],
   },
+  {
+    week: 3,
+    quiz: [
+      {
+        words: ["키보드", "마우스", "모니터", "스피커"],
+        description: "컴퓨터의 주변기기",
+      },
+      {
+        words: ["소다", "코코아", "스무디", "라떼"],
+        description: "음료의 종류",
+      },
+      {
+        words: ["아이", "피", "큐", "엑스"],
+        description: "알파벳",
+      },
+      {
+        words: ["바운스", "핀", "데드", "스노우"],
+        description: "__ 볼",
+      },
+    ],
+  },
+  {
+    week: 4,
+    quiz: [
+      {
+        words: ["장미", "매화", "모란", "벚꽃"],
+        description: "꽃",
+      },
+      {
+        words: ["타로", "신점", "점성술", "사주"],
+        description: "점술 방법",
+      },
+      {
+        words: ["무지", "매우", "아주", "겨우"],
+        description: "정도를 나타내는 표현",
+      },
+      {
+        words: ["사백", "이만", "백조", "사천"],
+        description: "숫자",
+      },
+    ],
+  },
+  {
+    week: 5,
+    quiz: [
+      {
+        words: ["퀸", "나이트", "룩", "폰"],
+        description: "체스 기물",
+      },
+      {
+        words: ["스콘", "머핀", "도넛", "파이"],
+        description: "빵의 종류",
+      },
+      {
+        words: ["체크", "콜", "폴드", "베트"],
+        description: "포커의 베팅 용어",
+      },
+      {
+        words: ["튜브", "콘", "무지", "네오"],
+        description: "카카오프렌즈",
+      },
+    ],
+  },
+  {
+    week: 6,
+    quiz: [
+      {
+        words: ["애플", "구글", "넷플릭스", "이케아"],
+        description: "다국적기업",
+      },
+      {
+        words: ["팬", "냄비", "솥", "웍"],
+        description: "조리용구",
+      },
+      {
+        words: ["카푸치노", "콜드브루", "드립", "루왁"],
+        description: "커피의 종류",
+      },
+      {
+        words: ["키", "화이트", "백", "스케이트"],
+        description: "__ 보드",
+      },
+    ],
+  },
 ];
